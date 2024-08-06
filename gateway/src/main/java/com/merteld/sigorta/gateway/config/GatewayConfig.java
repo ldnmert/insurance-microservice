0896ce189584cbf2d81b2cdeef19556ddafb741c
mert <merteldener7@gmail.com>
2024-08-06 13:22:25 +0300

    gateway cors config fixed

--- gateway/src/main/java/com/merteld/sigorta/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/merteld/sigorta/gateway/config/GatewayConfig.java
@@ -5,8 +5,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+//@CrossOrigin(origins = "*")
 public class GatewayConfig {
     private final JwtAuthenticationFilter filter;
 
@@ -16,14 +18,19 @@ public class GatewayConfig {
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
+
         return builder.routes()
                 .route("user-service", r -> r.path("/v1/user/**")
                         .filters(f -> f.filter(filter))
                         .uri("lb://user-service"))
 
-//                .route("job-service", r -> r.path("/v1/job-service/**")
-//                        .filters(f -> f.filter(filter))
-//                        .uri("lb://job-service"))
+                .route("vehicle-service", r -> r.path("/v1/vehicle/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://vehicle-service"))
+
+                .route("vehicle-service", r -> r.path("/v1/vehicleCode/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://vehicle-service"))
 
                 .route("policy-service", r -> r.path("/v1/policy/**")
                         .filters(f -> f.filter(filter))
@@ -31,6 +38,10 @@ public class GatewayConfig {
 
                 .route("auth-service", r -> r.path("/v1/auth/**")
                         .uri("lb://auth-service"))
+
+                .route("payment-service", r -> r.path("/v1/payment/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://payment-service"))
 
                 .route("customer-service", r -> r.path("/v1/customer/**")
                         .filters(f -> f.filter(filter))
